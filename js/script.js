8928c64e106e1423b43c3d4e4ff79114fb30e7f2
Michele Cofano <brutalmachinelol@gmail.com>
2021-01-31 23:30:29 +0100

    Aggiunta sidebar (aggiunti anche commenti)

--- js/script.js
+++ js/script.js
@@ -8,100 +8,108 @@
 // Copiate nel vostro data l'array di oggetti contacts, che trovate nella cartella drive,
 // oppure qui in allegato.
 
-new Vue ({
+var app = new Vue({
   el: '#app',
   data: {
-    // account utente
+// SEZIONE ACCOUNT UTENTE --------------------------
     user: {
-      name: 'Nome User',
-      avatar: '_io'
+        name: 'Nome Utente',
+        avatar: '_io'
     },
-    // contatti
+// SEZIONE CONTATTI --------------------------
     contacts: [
-	{
-		name: 'Michele',
-		avatar: '_1',
-		visible: true,
-		messages: [
-			{
-				date: '10/01/2020 15:30:55',
-				text: 'Hai portato a spasso il cane?',
-				status: 'sent'
-			},
-			{
-				date: '10/01/2020 15:50:00',
-				text: 'Ricordati di dargli da mangiare',
-				status: 'sent'
-			},
-			{
-				date: '10/01/2020 16:15:22',
-				text: 'Tutto fatto!',
-				status: 'received'
-			}
-		],
-	},
-	{
-		name: 'Fabio',
-		avatar: '_2',
-		visible: true,
-		messages: [
-			{
-				date: '20/03/2020 16:30:00',
-				text: 'Ciao come stai?',
-				status: 'sent'
-			},
-			{
-				date: '20/03/2020 16:30:55',
-				text: 'Bene grazie! Stasera ci vediamo?',
-				status: 'received'
-			},
-			{
-				date: '20/03/2020 16:35:00',
-				text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-				status: 'sent'
-			}
-		],
-	},
-	{
-		name: 'Samuele',
-		avatar: '_3',
-		visible: true,
-		messages: [
-			{
-				date: '28/03/2020 10:10:40',
-				text: 'La Marianna va in campagna',
-				status: 'received'
-			},
-			{
-				date: '28/03/2020 10:20:10',
-				text: 'Sicuro di non aver sbagliato chat?',
-				status: 'sent'
-			},
-			{
-				date: '28/03/2020 16:15:22',
-				text: 'Ah scusa!',
-				status: 'received'
-			}
-		],
-	},
-	{
-		name: 'Luisa',
-		avatar: '_4',
-		visible: true,
-		messages: [
-			{
-				date: '10/01/2020 15:30:55',
-				text: 'Lo sai che ha aperto una nuova pizzeria?',
-				status: 'sent'
-			},
-			{
-				date: '10/01/2020 15:50:00',
-				text: 'Si, ma preferirei andare al cinema',
-				status: 'received'
-			}
-		],
-	},
-]
+      {
+        name: 'Michele',
+        avatar: '_1',
+        visible: true,
+        messages: [
+            {
+                date: '10/01/2020 15:30:55',
+                message: 'Hai portato a spasso il cane?',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 15:50:00',
+                message: 'Ricordati di dargli da mangiare',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 16:15:22',
+                message: 'Tutto fatto!',
+                status: 'received'
+            },
+        ],
+      },
+      {
+        name: 'Fabio',
+        avatar: '_2',
+        visible: true,
+        messages: [
+            {
+                date: '20/03/2020 16:30:00',
+                message: 'Ciao come stai?',
+                status: 'sent'
+            },
+            {
+                date: '20/03/2020 16:30:55',
+                message: 'Bene grazie! Stasera ci vediamo?',
+                status: 'received'
+            },
+            {
+                date: '20/03/2020 16:35:00',
+                message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                status: 'received'
+            }
+        ],
+      },
+      {
+        name: 'Samuele',
+        avatar: '_3',
+        visible: true,
+        messages: [
+            {
+                date: '28/03/2020 10:10:40',
+                message: 'La Marianna va in campagna',
+                status: 'received'
+            },
+            {
+                date: '28/03/2020 10:20:10',
+                message: 'Sicuro di non aver sbagliato chat?',
+                status: 'sent'
+            },
+            {
+                date: '28/03/2020 16:15:22',
+                message: 'Ah scusa!',
+                status: 'received'
+            }
+        ],
+      },
+      {
+        name: 'Luisa',
+        avatar: '_4',
+        visible: true,
+        messages: [
+            {
+                date: '10/01/2020 15:30:55',
+                message: 'Lo sai che ha aperto una nuova pizzeria?',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 15:50:00',
+                message: 'Si, ma preferirei andare al cinema',
+                status: 'received'
+            }
+        ],
+      },
+    ],
+    indexContact: 0
+// SEZIONE CONTATTI --------------------------
+  },
+  methods: { // funzioni
+      setIndexContact(index) {
+          console.log(index);
 
-  }
-})
+          this.indexContact = index;
+      }
+    }
+});
